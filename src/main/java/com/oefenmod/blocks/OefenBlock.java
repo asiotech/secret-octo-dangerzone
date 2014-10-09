ef2b83d710e68c53b74ca481eb7bf2b10525e92a
asiotech <asiotech@live.nl>
2014-10-09 23:29:37 +0200

    Remove example, add new classes

---
+++ src/main/java/com/oefenmod/blocks/OefenBlock.java
@@ -0,0 +1,12 @@
+package com.oefenmod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class OefenBlock extends Block
+{
+	public OefenBlock (Material material)
+	{
+		super(material);
+	}
+}
