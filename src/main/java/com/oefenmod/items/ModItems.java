1d6934b45178c856cfdd3545d205a37c6512896e
asiotech <asiotech@live.nl>
2014-10-10 18:11:24 +0200

    Adds proxy classes and implements in OefenMod, adds first steps of adding Items

---
+++ src/main/java/com/oefenmod/items/ModItems.java
@@ -0,0 +1,13 @@
+package com.oefenmod.items;
+
+import net.minecraft.item.Item;
+
+public final class ModItems {
+	
+	public static Item oefenItem;
+	
+	public static final void init()
+	{
+		oefenItem = new Item();
+	}
+}
--- src/main/java/com/oefenmod/oefenmod/OefenMod.java
+++ src/main/java/com/oefenmod/oefenmod/OefenMod.java
@@ -3,6 +3,7 @@ package com.oefenmod.oefenmod;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -15,36 +16,44 @@ public class OefenMod
 	public static final String VERSION = "0.0.1";
 	public static final String NAME = "Oefen";
 
-	 @Instance
-     public static OefenMod instance = new OefenMod();
+	@SidedProxy(clientSide="com.oefenmod.oefenmod.ClientProxy", serverSide="com.oefenmod.oefenmod.ServerProxy")
+	public static CommonProxy proxy;
 	
-	 /**
-      * Run before anything else. Read your config, create blocks, items, etc, and 
-      * register them with the GameRegistry.
-      */
-     @EventHandler
-     public void preInit(FMLPreInitializationEvent e) 
-     {
-    	 System.out.println("Called method: preInit");
-     }
+	@Instance
+    public static OefenMod instance = new OefenMod();
+	
+	
+	
+	/**
+    * Run before anything else. Read your config, create blocks, items, etc, and 
+    * register them with the GameRegistry.
+    */
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) 
+    {
+    	System.out.println("Called method: preInit");
+    	proxy.preInit(e);
+    } 
 	 
-     /**
-      * Do your mod setup. Build whatever data structures you care about. Register recipes.
-      * Register new Handler
-      */
-     @EventHandler
-     public void init(FMLInitializationEvent e) 
-     {
-    	 System.out.println("Called method: init");
-     }
+    /**
+    * Do your mod setup. Build whatever data structures you care about. Register recipes.
+    * Register new Handler
+    */
+    @EventHandler
+    public void init(FMLInitializationEvent e) 
+    {
+    	System.out.println("Called method: init");
+    	proxy.init(e);
+    }
 	
-     /**
-      * Handle interaction with other mods, complete your setup based on this.
-      */
-     @EventHandler
-     public void postInit(FMLPostInitializationEvent e) 
-     {
-    	 System.out.println("Called method: postInit");
-     }
+    /**
+     * Handle interaction with other mods, complete your setup based on this.
+     */
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e) 
+    {
+    	System.out.println("Called method: postInit");
+    	proxy.postInit(e);
+    }
 	
 }
